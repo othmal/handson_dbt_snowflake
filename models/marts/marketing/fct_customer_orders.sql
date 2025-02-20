68b8f65932a3bac460eb861016289bb2e39fb921
OTHMAN MALOUANE <othman.malouane@devoteam.com>
2025-02-20 09:43:27 +0000

    add compare package

--- models/marts/marketing/fct_customer_orders.sql
+++ models/marts/marketing/fct_customer_orders.sql
@@ -1,44 +1,47 @@
-WITH customers AS (
-    SELECT * FROM {{ ref('stg_jaffle_shop__customers') }}
-)
-,int_orders AS (
-    SELECT * FROM {{ ref('int_orders') }}
-)
-,customer_orders AS (
-    SELECT 
-        customer_id
-        ,order_id
-        ,order_date
-        ,order_status
-        ,total_amount_paid
-        ,payment_finalized_date
-        ,ROW_NUMBER()    OVER (PARTITION BY customer_id ORDER BY order_id) as customer_sales_seq
-        ,ROW_NUMBER()    OVER (ORDER BY order_id) as transaction_seq
-        ,min(order_date) OVER (PARTITION BY customer_id) AS first_order_date
-        --,max(order_date) OVER (PARTITION BY customer_id) AS most_recent_order_date
-        --,count(order_id) OVER (PARTITION BY customer_id) AS number_of_orders
-        ,sum(total_amount_paid) OVER (PARTITION BY customer_id) AS lifetime_value
-        ,order_date
-     FROM int_orders    
-)
-,final AS (
-    SELECT 
-        customers.customer_id,
-        customer_orders.order_id,
-        customer_orders.order_date,
-        customer_orders.order_status,
-        customer_orders.total_amount_paid,
-        customer_orders.payment_finalized_date,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.transaction_seq,
-        customer_orders.customer_sales_seq,
-        customer_orders.first_order_date,
-        CASE WHEN customer_orders.first_order_date = customer_orders.order_date THEN 'new' ELSE 'return' END as nvsr,
-        coalesce(customer_orders.lifetime_value,0) AS customer_lifetime_value,
-        customer_orders.first_order_date as fdos
-    FROM customer_orders
-    left join customers using (customer_id)
-)
+WITH paid_orders as (select Orders.ID as order_id,
+    Orders.USER_ID	as customer_id,
+    Orders.ORDER_DATE AS order_placed_at,
+        Orders.STATUS AS order_status,
+    p.total_amount_paid,
+    p.payment_finalized_date,
+    C.FIRST_NAME    as customer_first_name,
+        C.LAST_NAME as customer_last_name
+FROM RAW.SNOWFLAKE_DB_RAW_JAFFLE_SHOP.ORDERS as Orders
+left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
+        from raw.SNOWFLAKE_DB_RAW_STRIPE.payment
+        where STATUS <> 'fail'
+        group by 1) p ON orders.ID = p.order_id
+left join raw.SNOWFLAKE_DB_RAW_JAFFLE_SHOP.customers C on orders.USER_ID = C.ID ),
 
-SELECT * FROM final ORDER BY customer_id,order_id 
+customer_orders 
+as (select C.ID as customer_id
+    , min(ORDER_DATE) as first_order_date
+    , max(ORDER_DATE) as most_recent_order_date
+    , count(ORDERS.ID) AS number_of_orders
+from raw.SNOWFLAKE_DB_RAW_JAFFLE_SHOP.customers C 
+left join raw.SNOWFLAKE_DB_RAW_JAFFLE_SHOP.orders as Orders
+on orders.USER_ID = C.ID 
+group by 1)
+
+select
+p.*,
+ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
+ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
+CASE WHEN c.first_order_date = p.order_placed_at
+THEN 'new'
+ELSE 'return' END as nvsr,
+x.clv_bad as customer_lifetime_value,
+c.first_order_date as fdos
+FROM paid_orders p
+left join customer_orders as c USING (customer_id)
+LEFT OUTER JOIN 
+(
+        select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+    from paid_orders p
+    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+) x on x.order_id = p.order_id
+ORDER BY order_id
